17e5da748085b02f9270211c4ef0cbf796778d04
Gabriel Gutierrez <gabrielgutierrezcorral@hotmail.com>
2016-01-18 22:15:13 +0100

    Assignment2 Coursera
    Programming Assignment2 Coursera DS Specialization

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This code is able to cache the value of the inverse of a given matrix
+## to optimize the computation time (if the content is not
+## changing), taking advantage of the scoping rules.
 
-## Write a short comment describing this function
+## This function contains a listreates a special "vector",
+## which is really a list containing a function to:
+## - set the value of the matrix
+## - get the value of the matrix
+## - set the value of the inverse
+## - get the value of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y){
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(solve) m <<- solve
+        getinverse <- function() m
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
+## This function returns a matrix that is the inverse of 'x',
+## getting it from the cache data if possible.
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x) {
+        m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data)
+        x$setinverse(m)
+        m
 }
